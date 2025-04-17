9d285084e21bfeb5318dfb77a5217dfbc93dfe05
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-04-16 23:07:38 -0300

    Initial commit
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

---
+++ PayloadParser.js
@@ -0,0 +1,127 @@
+function parseUplink(device, payload)
+{
+    // Obtener payload como JSON
+    const jsonPayload = payload.asJsonObject();
+    Object.keys(jsonPayload).forEach(function(key){
+        env.log(key,jsonPayload[key])
+    })
+
+    // No se puede deserializar el payload como json, salir.
+    if (!jsonPayload) { return; }
+
+    // Procesar JSON de EZE GEN1 (hasta 200 registros por sensor por call)
+
+
+    //----------------------------------------------------------------------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------------------------------------
+
+
+    // DEclaracion de variable para todos los endpoints
+
+    if (jsonPayload.data !== null) {
+    //Variables LOE ABAJO
+        var xileno = device.endpoints.byAddress(1);
+        var tolueno = device.endpoints.byAddress(2);
+        var aguarras3 = device.endpoints.byAddress(3);
+
+        //----------------------------------------------------------
+        
+         // TANQUE XILENO
+        if (jsonPayload.pid == "E1-D0470-xilol")
+        {
+            const data = jsonPayload.data;
+            var ultimo = "";
+                for (let i = 0; i < data.length; i++) {
+                    const item = data[i];
+                    env.log("FECHA --> ",item.time);
+                    env.log("VOL --> ",item.val);
+                    xileno.updateVolumeSensorStatus(item.val,item.time);
+                    // DDM: agrego este "if" por si los datos vinieran desordenados
+                    if (item.time > ultimo)
+                    ultimo = item.time;
+                    }
+                env.log("Ultimo --> ", ultimo);
+
+        // Resultado del método HTTP. Devolvemos un json con un campo "result", que
+        // contiene el "time" más alto recibido, como se indica en la documentación.
+        var httpResponse = new HttpResponse();
+        httpResponse.statusCode = 200;
+        httpResponse.contentType = "application/json";
+        httpResponse.content.setAsJson({ result: ultimo });
+        return httpResponse;
+        }
+
+
+        /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        --------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+        // Tolueno
+
+        if (jsonPayload.pid == "E2 Tolueno")
+        {
+            const data = jsonPayload.data;
+            var ultimo = "";
+                for (let i = 0; i < data.length; i++) {
+                    const item = data[i];
+                    env.log("FECHA --> ",item.time);
+                    env.log("VOL --> ",item.val);
+                    tolueno.updateVolumeSensorStatus(item.val,item.time);
+                    // DDM: agrego este "if" por si los datos vinieran desordenados
+                    if (item.time > ultimo)
+                    ultimo = item.time;
+                    }
+                env.log("Ultimo --> ", ultimo);
+
+        // Resultado del método HTTP. Devolvemos un json con un campo "result", que
+        // contiene el "time" más alto recibido, como se indica en la documentación.
+        var httpResponse = new HttpResponse();
+        httpResponse.statusCode = 200;
+        httpResponse.contentType = "application/json";
+        httpResponse.content.setAsJson({ result: ultimo });
+        return httpResponse;
+        }
+
+
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+        // Aguarras Mineral
+        
+        if (jsonPayload.pid == "E3-D0462-aguarras")
+        {
+            const data = jsonPayload.data;
+            var ultimo = "";
+                for (let i = 0; i < data.length; i++) {
+                    const item = data[i];
+                    env.log("FECHA --> ",item.time);
+                    env.log("VOL --> ",item.val);
+                    aguarras3.updateVolumeSensorStatus(item.val,item.time);
+                    // DDM: agrego este "if" por si los datos vinieran desordenados
+                    if (item.time > ultimo)
+                    ultimo = item.time;
+                    }
+                env.log("Ultimo --> ", ultimo);
+
+        // Resultado del método HTTP. Devolvemos un json con un campo "result", que
+        // contiene el "time" más alto recibido, como se indica en la documentación.
+        var httpResponse = new HttpResponse();
+        httpResponse.statusCode = 200;
+        httpResponse.contentType = "application/json";
+        httpResponse.content.setAsJson({ result: ultimo });
+        return httpResponse;
+    }
+
+
+
+
+
+    }
+    
+    // Importante: si el script llega a este punto es que no se pudo parsear el json.
+    // Como no estamos devolviendo nada, el motor de scripting devolverá el valor por
+    // defecto, que consiste en:
+    // - Status code 200
+    // - Content type "text/plain"
+    // - Body vacío (sin contenido)
+}
